23d069faf8bda1be762e5c7e9165fa08f49cd4df
Nahomie ND <nducasse3@gmail.com>
2020-12-05 12:29:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,12 +11,17 @@ const increasedRankBy = (n) => {
 }
 
 function deepestChild() {
-  let node = $()             
+  let node = $('#grand-node'); 
+  let nextNode = node.children[0];
+  
+  while (nextNode){
+    node = nextNode;
+    nextNode = node.children[0];
+  }
 }
 
 function deepestChild() {
-  let node = document.getElementById('grand-node')
-  let nextNode = node.children[0]
+
   let node = document.getElementById('grand-node');
   let nextNode = node.children[0];
 
