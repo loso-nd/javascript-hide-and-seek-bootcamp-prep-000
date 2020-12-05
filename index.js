afb190f6418ebceca4836f3344ba33b955c015de
Nahomie ND <nducasse3@gmail.com>
2020-12-05 12:26:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,22 @@ const increasedRankBy = (n) => {
 }
 
 function deepestChild() {
-                  
+  let node = $()             
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+
+  return node
+  return node;
 }
